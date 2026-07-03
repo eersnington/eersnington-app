c6312d9d1615108fea215e9cbaa7efc9bfb7db7d
Sree Narayanan <sreeaadhi07@gmail.com>
2026-07-03 15:01:00 +0400

    Allow turbotunnel host

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,4 +4,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
+  server: {
+    host: true,
+    allowedHosts: [
+      'ttdo5kud-turbotunnel.vercel.app',
+    ]
+  }
 });
